e585e74f0ad6717cfc6d8f61b1bac17ea79b0ceb
jmathen2 <jmathen2@mix.wvu.edu>
2016-02-23 15:27:43 -0500

    "Attendance" Assignment

--- Attendance/Attendance/Student.m
+++ Attendance/Attendance/Student.m
@@ -5,15 +5,16 @@
 //  Created by Ricky Kirkendall on 1/31/16.
 //  Copyright © 2016 CodeLab. All rights reserved.
 //
+//  Josh Matheny
 
 #import "Student.h"
 
 @implementation Student
 
-- (NSString *)description
+- (NSString *) description;
 {
     //TODO: Complete this method
-    return [NSString stringWithFormat:@"%@ %@, #%@", self.firstName, @"",@""];
+    return [NSString stringWithFormat:@"%@ %@, #%@", self.firstName, self.lastName, self.studentId];
 }
 
 @end
--- Attendance/Attendance/main.m
+++ Attendance/Attendance/main.m
@@ -5,6 +5,7 @@
 //  Created by Ricky Kirkendall on 1/31/16.
 //  Copyright © 2016 CodeLab. All rights reserved.
 //
+//Josh Matheny
 
 #import <Foundation/Foundation.h>
 
@@ -13,14 +14,28 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
+        
+        Student *josh =[[Student alloc]init];
+        Student *michael =[[Student alloc]init];
+        Student *optimus =[[Student alloc]init];
+        
+        [josh setFirstName:@"Josh"];
+        [josh setLastName:@"Matheny"];
+        [josh setStudentId:@"9000999"];
+        
+        [michael setFirstName:@"Michael"];
+        [michael setLastName:@"Jordan"];
+        [michael setStudentId:@"9002323"];
+        
+        [optimus setFirstName:@"Optimus"];
+        [optimus setLastName:@"Prime"];
+        [optimus setStudentId:@"1111111"];
+        
         
         Course *codeLab = [[Course alloc]init];
-        
-        // TODO: Create at least 2 student objects
-        // TODO: Add students to the codeLab Course
-        
-
+        [codeLab.students addObject:josh];
+        [codeLab.students addObject:michael];
+        [codeLab.students addObject:optimus];
         
         NSLog(@"%@",[codeLab roleCall]);
         
